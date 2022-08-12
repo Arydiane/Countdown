a2c41a9ff152f6d25f60ad0929e4a64bf7b95f0d
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-08-12 19:56:28 -0300

    javascript da janela modal

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModal() {
+    const modal = document.getElementById('modal-window');
+    modal.classList.add('active');
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'close' || e.target.id == 'modal-window' )
+        modal.classList.remove('active');
+    })
+}
