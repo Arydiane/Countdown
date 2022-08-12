b23b0d9f37c9bea5be317e4abd8d833fb684eb3a
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-08-12 20:04:16 -0300

    atualização javascript janela modal

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function openModal() {
     modal.classList.add('active');
 
     modal.addEventListener('click', (e) => {
-        if (e.target.id == 'close' || e.target.id == 'modal-window' )
-        modal.classList.remove('active');
+        if (e.target.id == 'close' || e.target.id == 'modal-window' ){
+            modal.classList.remove('active');
+        } 
     })
 }
