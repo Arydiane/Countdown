903db056a8624892845545dc54282675a0ecfeda
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-08-13 14:50:55 -0300

    atualizei a função countdown
    Verifica se a data do lançamento já passou e avisa com uma mensagem.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
+const container = document.querySelector('.countdown-container');
+
 
 const launch = "13 Aug 2024";
 
@@ -15,18 +17,24 @@ function countdown() {
     const oneMinute = 60;
     const oneSecond = 1000;
 
-    const totalseconds = (launchDate - currentDate) / oneSecond;
+    if ( launchDate > currentDate) {
 
-    const days = Math.floor(totalseconds / oneDay);
-    const hours = Math.floor(totalseconds / oneHour) % 24;
-    const minutes = Math.floor(totalseconds / oneMinute) % 60;
-    const seconds = Math.floor(totalseconds) % 60;
+        const totalseconds = (launchDate - currentDate) / oneSecond;
 
-    daysEl.innerHTML = days; 
-    hoursEl.innerHTML = `: ${formatTime(hours)}`;
-    minutesEl.innerHTML = `: ${formatTime(minutes)}`;
-    secondsEl.innerHTML = `: ${formatTime(seconds)}`;
-
+        const days = Math.floor(totalseconds / oneDay);
+        const hours = Math.floor(totalseconds / oneHour) % 24;
+        const minutes = Math.floor(totalseconds / oneMinute) % 60;
+        const seconds = Math.floor(totalseconds) % 60;
+    
+        daysEl.innerHTML = days; 
+        hoursEl.innerHTML = `: ${formatTime(hours)}`;
+        minutesEl.innerHTML = `: ${formatTime(minutes)}`;
+        secondsEl.innerHTML = `: ${formatTime(seconds)}`;
+    } else {
+        container.innerHTML = "O lançamento já foi realizado!"; 
+        container.style.fontSize=30+"px"; 
+    }
+    
 }
 
 function formatTime (time) {
